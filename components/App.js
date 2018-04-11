19f747bf58a151585b9c2e01282961383c4f6f56
Marcin Bogdanski <marcin@bogdanski.info>
2018-04-11 08:27:29 +0200

    some issue

--- components/App.js
+++ components/App.js
@@ -1,5 +1,10 @@
+var GIPHY_PUB_KEY = 'gJ8TFfCdfwquNY24Qp53zvFhGs05Pz3y',
+  GIPHY_API_URL = 'https://api.giphy.com'
+
+const url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag='
+
 App = React.createClass({
-  
+
   getInitialState () {
     return {
       loading: false,
@@ -7,67 +12,57 @@ App = React.createClass({
       gif: {}
     }
   },
-  
+
+  getGifPromise: function (searchingText) {
+    return new Promise(function (resolve, reject) {
+      const xhr = new XMLHttpRequest()
+      xhr.onload = function () {
+        if (xhr.status === 200) {
+          var data = JSON.parse(xhr.responseText).data
+
+          var gif = {
+            url: data.fixed_width_downsampled_url,
+            sourceUrl: data.url
+          }
+          resolve(gif)
+        } else { reject(new Error(this.statusText)) }
+      }
+      xhr.open('GET', url + searchingText)
+      xhr.send()
+    })
+  },
+
   handleSearch: function (searchingText) {
     this.setState({
       loading: true
     })
-    
-    this.getGif(searchingText, function (gif) {
-      this.setState({ 
+
+    this.getGifPromise(searchingText).then(function (gif) {
+      this.setState({
         loading: false,
         gif: gif,
         searchingText: searchingText
       })
-    }.bind(this)) 
+    }.bind(this))
   },
-  
-  getGif: function(searchingText) {
-    return new Promise (
-      (resolve, reject) => {
-        const GIPHY_PUB_KEY = 'gJ8TFfCdfwquNY24Qp53zvFhGs05Pz3y';
-        const GIPHY_API_URL = 'https://api.giphy.com';
-        let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.onload = function() {
-          if (this.status === 200) {
-            let data = JSON.parse(xhr.responseText).data;
-            let gif = {
-              url: data.fixed_width_downsampled_url,
-              sourceUrl: data.url
-            };
-            resolve(gif);
-          }
-          reject(new Error(this.statusText));
-        };
-        xhr.onerror = function () {
-          reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
-        };
-        xhr.send();
-      }
-    );
-  },
-  
+
   render: function () {
     var styles = {
       margin: '0 auto',
       textAlign: 'center',
-      width: '60%'
+      width: '90%'
     }
-    
+
     return (
       <div style={styles}>
-      <h1>Wyszukiwarka GIFów!</h1>
-      <p>Znajdź GIFy na: <a href='http://giphy.com'>
-      giphy.com</a> <br /> Wciśnij enter aby załadować więcej.
-      </p>
-      <Search onSearch={this.handleSearch} />
-      <Gif
-      loading={this.state.loading}
-      url={this.state.gif.url}
-      sourceUrl={this.state.gif.sourceUrl}
-      />
+        <h1>Wyszukiwarka GIFow!</h1>
+        <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+        <Search onSearch={this.handleSearch} />
+        <Gif
+          loading={this.state.loading}
+          url={this.state.gif.url}
+          sourceUrl={this.state.gif.sourceUrl}
+        />
       </div>
     )
   }
